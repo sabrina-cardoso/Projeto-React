ce8e001cf18f0044ee89b18a362c9637ffd0ee5c
Sabrina <sabrina.o.cardoso@outlook.com>
2023-05-19 23:36:15 -0300

    arrumando bugs

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ function App() {
   const[colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
 
   }
