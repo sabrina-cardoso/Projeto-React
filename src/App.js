801ab75d30cec6f649c60dd0f688d6cbc7dec2d0
Sabrina <sabrina.o.cardoso@outlook.com>
2023-05-19 22:15:19 -0300

    novos componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9',
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+    },
+    {
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    }
+  ]
+
   const[colaboradores, setColaboradores] = useState([])
+
   const aoNovoColaboradorAdicionado = (colaborador) => {
     console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
@@ -14,7 +39,14 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      {times.map(time => 
+      <Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria}
+      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}/>)}
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,26 +6,23 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'Mobile'
-    ]
-
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
 
-
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log(nome, cargo, imagem)
         props.aoColaboradorCadastrado({
-            nome, cargo, imagem, time
+            nome,
+            cargo,
+            imagem,
+            time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
@@ -33,29 +30,35 @@ const Formulario = (props) => {
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto 
-                    obrigatorio={true} 
-                    label="Nome" 
-                    placeholder="Digite seu nome"
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome" 
                     valor={nome}
-                    aoAlterado={valor => setNome(valor)}/>
-                <CampoTexto obrigatorio={true} 
-                    label="Cargo" 
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
                     placeholder="Digite seu cargo" 
-                    valor={cargo} 
-                    aoAlterado={valor => setCargo(valor)} />
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Imagem" 
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    label="Imagem"
                     placeholder="Digite o endereço da imagem" 
-                    valor={imagem} 
-                    aoAlterado={valor => setImagem(valor)} />
-                <ListaSuspensa 
-                    obrigatorio={true} 
-                    label ="Time" 
-                    itens={times} 
-                    valor={time} 
-                    aoAlterado={valor => setTime(valor)}/>
-                <Botao>Criar Card</Botao>
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time" 
+                    itens={props.times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
